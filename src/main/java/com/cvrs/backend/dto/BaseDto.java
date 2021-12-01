d472a087b830c44b00b2fe9d2c490317e6f38cf5
Bijen Shrestha <bijenshrestha@JUSTABOOK.domain.name>
2021-12-01 16:54:52 +0545

    add: updated base

--- src/main/java/com/cvrs/backend/dto/BaseDto.java
+++ src/main/java/com/cvrs/backend/dto/BaseDto.java
@@ -1,4 +1,21 @@
 package com.cvrs.backend.dto;
 
-public class BaseDto {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class BaseDto implements Serializable {
+    private Long id;
+
+    private Long createdBy;
+
+    private Long updatedBy;
+
+    private Date createdDate;
+
+    private String name;
 }
--- src/main/java/com/cvrs/backend/entity/BaseEntity.java
+++ src/main/java/com/cvrs/backend/entity/BaseEntity.java
@@ -1,15 +1,35 @@
 package com.cvrs.backend.entity;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Date;
 
-public class BaseEntity {
+@Getter
+@Setter
+public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "created_by", updatable = false)
     private Long createdBy;
 
+    @Column(name = "updated_by", updatable = true)
     private Long updatedBy;
 
+    @Column(name = "created_date", updatable = false)
     private Date createdDate;
 
+    @Column(name = "name")
     private String name;
 
 }
