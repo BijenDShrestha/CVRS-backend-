6314fe3f89aebb30c1b5a5b26b1d5ef12c50ab0a
sanam uash <udashsanam@gmail.com>
2021-12-31 20:38:51 +0545

    add: ip address

--- src/main/java/com/cvrs/backend/controller/base/BaseController.java
+++ src/main/java/com/cvrs/backend/controller/base/BaseController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = {
         "http://localhost:3000",
-        "http://192.168.1.2:3000"
+        "http://192.168.1.2:3000",
+        "http://192.169.1.1:3000"
 })
 public class BaseController {
+
 }
