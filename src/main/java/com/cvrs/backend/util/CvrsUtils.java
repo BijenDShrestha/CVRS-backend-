e43466c50da26c6c9f775bb77076d70ad5244381
Bijen Shrestha <bijenshrestha@JUSTABOOK.local>
2022-01-03 14:32:52 +0545

    add: status code for CVRS

--- src/main/java/com/cvrs/backend/util/CvrsUtils.java
+++ src/main/java/com/cvrs/backend/util/CvrsUtils.java
@@ -1,4 +1,18 @@
 package com.cvrs.backend.util;
 
 public interface CvrsUtils {
+    // For Age Category
+    static final String CHILDREN = "1";
+    static final String TEEN = "2";
+    static final String YOUNG_ADULT = "3";
+    static final String MIDDLE_AGED_ADULT = "4";
+    static final String OLD_ADULT = "5";
+
+    // For Vaccine Status
+    static final String PENDING = "pending";
+    static final String GOT_ONE = "got one";
+    static final String COMPLETE = "complete";
+
+
+
 }
