d9577ca98dfe89539b7435f7475e4f041ddd7a3a
Bijen Shrestha <bijenshrestha@JUSTABOOK.local>
2022-01-30 20:31:45 +0545

    add: authorization

---
+++ src/main/java/com/cvrs/backend/controller/AuthenticationController.java
@@ -0,0 +1,68 @@
+package com.cvrs.backend.controller;
+
+import com.cvrs.backend.controller.base.BaseController;
+import com.cvrs.backend.dto.CustomDto.ResponseDto;
+import com.cvrs.backend.entity.AdminEntity;
+import com.cvrs.backend.repository.AdminRepository;
+import com.cvrs.backend.security.jwt.JwtToken;
+import com.cvrs.backend.security.model.JwtRequest;
+import com.cvrs.backend.security.model.JwtResponse;
+import com.cvrs.backend.security.services.CvrsUserDetailsService;
+import com.cvrs.backend.util.BCryptUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController extends BaseController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtToken jwtToken;
+
+    @Autowired
+    private CvrsUserDetailsService cvrsUserDetailsService;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception{
+        try {
+//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+            AdminEntity adminEntity = adminRepository.findByUserName(authenticationRequest.getUsername());
+            authenticate(adminEntity, authenticationRequest.getPassword());
+
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = cvrsUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtToken.generateToken(userDetails);
+
+//        return new ResponseEntity<>(new ResponseDto("Authentication Successful", new JwtResponse(token)), HttpStatus.OK);
+        return ResponseEntity.ok(new JwtResponse(token));
+
+    }
+
+    private boolean authenticate(final AdminEntity adminEntity, String password) {
+        if (BCryptUtil.match(password, adminEntity.getPassword())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
--- src/main/java/com/cvrs/backend/repository/AdminRepository.java
+++ src/main/java/com/cvrs/backend/repository/AdminRepository.java
@@ -4,6 +4,11 @@ import com.cvrs.backend.entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
+//    AdminEntity findAdminEntityByUserName(String username);
+
+    AdminEntity findByUserName(String username);
 }
